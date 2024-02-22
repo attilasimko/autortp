63cb82e9868e14bec9bc71e0c27045f164f316e8
Attila <attila.simko@umu.se>
2024-02-22 13:38:37 +0100

    go

--- model.py
+++ model.py
@@ -10,24 +10,15 @@ def build_model(batch_size=1, num_cp=6):
     inp = Input(shape=(64, 64, 64, 2), dtype=tf.float16, batch_size=batch_size)
     x = Conv3D(4, (3, 3, 3), activation='relu', padding='same')(inp)
     x = Conv3D(32, (3, 3, 3), activation='relu', padding='same')(x)
-    x = MaxPooling3D((2, 2, 2))(x)
+    x = MaxPooling3D((4, 4, 4))(x)
     x = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(x)
     x = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(x)
-    x = MaxPooling3D((2, 2, 2))(x)
+    x = MaxPooling3D((4, 4, 4))(x)
     x = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(x)
     x = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(x)
-    x = MaxPooling3D((2, 2, 2))(x)
-    x = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(x)
-    x = Conv3D(256, (3, 3, 3), activation='relu', padding='same')(x)
-    x = MaxPooling3D((2, 2, 2))(x)
-    x = Conv3D(512, (3, 3, 3), activation='relu', padding='same')(x)
-    x = Conv3D(512, (3, 3, 3), activation='relu', padding='same')(x)
-    x = MaxPooling3D((2, 2, 2))(x)
-    x = Conv3D(512, (3, 3, 3), activation='relu', padding='same')(x)
-    x = Conv3D(512, (3, 3, 3), activation='relu', padding='same')(x)
-    x = MaxPooling3D((2, 2, 2))(x)
-    x = Conv3D(512, (3, 3, 3), activation='relu', padding='same')(x)
-    x = Conv3D(512, (3, 3, 3), activation='relu', padding='same')(x)
+    x = MaxPooling3D((4, 4, 4))(x)
+    x = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(x)
+    x = Conv3D(128, (3, 3, 3), activation='relu', padding='same')(x)
     latent_space = Flatten()(x)
 
 
@@ -42,18 +33,15 @@ def monaco_plan(ct, latent_space, num_cp):
     dose = tf.zeros_like(ct)
     leafs = []
     for i in range(num_cp):
-        leaf = Dense(512, activation='relu')(latent_space)
-        leaf = Dense(256, activation='relu')(leaf)
-        leaf = Dense(128, activation='relu')(leaf)
+        leaf = Dense(128, activation='relu')(latent_space)
         leaf = Dense(128, activation='relu')(leaf)
         leafs.append(Reshape((2, 64))(leaf))
     
     mus = []
     for i in range(num_cp):
-        mu = Dense(512, activation='relu')(latent_space)
-        mu = Dense(256, activation='relu')(mu)
-        mu = Dense(128, activation='relu')(mu)
+        mu = Dense(128, activation='relu')(latent_space)
         mu = Dense(32, activation='relu')(mu)
+        mu = Dense(8, activation='relu')(mu)
         mu = Dense(1, activation='relu')(mu)
         mus.append(mu)
 
