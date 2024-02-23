193b333490f83cf0d03767bf35b32c7e2d3901ea
Attila <attila.simko@umu.se>
2024-02-23 15:57:16 +0100

    go

--- data.py
+++ data.py
@@ -5,11 +5,11 @@ def generate_data(center=None, sigma=None):
     shape = (64, 64, 64)
     ct = np.ones(shape, dtype=float)
     if sigma is None:
-        sigma = np.random.randint(5, 20)
+        sigma = np.random.randint(1, 30)
     if center is None:
-        center = np.random.randint(0, 64, size=2)
+        center = (32, 32, 32) # np.random.randint(0, 64, size=2)
     x, y, z = np.meshgrid(np.arange(shape[0]), np.arange(shape[1]), np.arange(shape[2]))
-    d2 = (x - center[0])**2 + (y - center[1])**2 + (z - 32)**2
+    d2 = (x - center[0])**2 + (y - center[1])**2 + (z - center[2])**2
     gaussian = np.exp(-d2 / (2 * sigma**2))
     gaussian /= np.max(gaussian)
     mask = np.array(gaussian > 0.5, dtype=float)
