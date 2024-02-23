bece55e3d7e98291d11928119eaa4de17af17059
Attila <attila.simko@umu.se>
2024-02-23 13:18:07 +0100

    go

--- losses.py
+++ losses.py
@@ -13,8 +13,8 @@ def rtp_loss(num_cp):
             mc_point = tf.random.uniform([3], 0, 64, dtype=tf.int32)
             true_dose = y_true[mc_point[0], mc_point[1], mc_point[2]]
             for cp_idx in range(num_cp):
-                ray_idx = ray_matrices[cp_idx][mc_point[0], mc_point[1], mc_point[2]]
                 absorption_value = absorption_matrices[cp_idx][mc_point[0], mc_point[1], mc_point[2]]
+                ray_idx = ray_matrices[cp_idx][mc_point[0], mc_point[1], mc_point[2]]
                 cond_leafs = tf.logical_and(tf.less_equal(leafs[:, 1, mc_point[1], cp_idx], ray_idx), tf.greater_equal(leafs[:, 0, mc_point[1], cp_idx], ray_idx))
                 dose += tf.reduce_sum(tf.where(cond_leafs, mus[:, 0, cp_idx], 0) * absorption_value)
             dose_diffs += tf.abs(true_dose - dose) / num_mc
