5ecea804cf1b3d6f895f33a36061ddb67454e97e
Attila <attila.simko@umu.se>
2024-02-25 22:08:10 +0100

    go

--- losses.py
+++ losses.py
@@ -8,7 +8,7 @@ def rtp_loss(num_cp, num_mc, leaf_length):
         leafs, mus = vector_to_monaco_param(y_pred, leaf_length, num_cp)
         dose_diffs = tf.cast([0.0], tf.float16)
         for batch_idx in range(y_true.shape[0]):
-            dose_values = tf.unique(tf.reshape(y_true[batch_idx, ...], [-1]))
+            dose_values = tf.cast(tf.unique(tf.reshape(y_true[batch_idx, ...], [-1])), tf.float16)
             for dose_value in dose_values:
                 boolean_array = tf.equal(y_true[batch_idx, ...], dose_value)
                 true_indices = tf.where(boolean_array)
@@ -17,7 +17,7 @@ def rtp_loss(num_cp, num_mc, leaf_length):
                     # mc_point = tf.random.uniform([3], 0, 64, dtype=tf.int32)
                     true_dose = y_true[:, mc_point[0], mc_point[1], mc_point[2]]
                     pred_dose = get_dose_value(absorption_matrices, ray_matrices, leafs, mus, mc_point)
-                    dose_diffs += tf.cast(tf.abs(true_dose - pred_dose) / num_mc, tf.float16)
+                    dose_diffs += tf.cast(tf.abs(dose_value - pred_dose) / num_mc, tf.float16)
 
         return dose_diffs
     return loss_fn
