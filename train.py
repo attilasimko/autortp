f0de0563db32b6225d2c6323751ea4e0ef6a1ea3
Attila <attila.simko@umu.se>
2024-02-25 14:51:51 +0100

    go

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ tf.compat.v1.enable_eager_execution()
 
 
 n_epochs = 100
-epoch_length = 500
+epoch_length = 100
 batch_size = 1
 
 # Number of control points
@@ -23,7 +23,7 @@ num_cp = 6
 # Length of each leaf
 leaf_length = 64
 # Number of Monte Carlo points
-num_mc = 10000
+num_mc = 50000
 # Generate ray matrices for each control point
 ray_matrices = get_monaco_projections(num_cp)
 
