8c53db355a179e35c6e535608bca27968dfd1aaf
Attila <attila.simko@umu.se>
2024-03-14 18:21:25 +0100

    whew

--- model.py
+++ model.py
@@ -14,12 +14,14 @@ def build_model(batch_size=1, num_cp=6):
     x = MaxPooling3D((4, 4, 4))(x)
     x = Conv3D(2 * 128, (3, 3, 3), activation='relu', padding='same', kernel_initializer="he_normal")(x)
     x = MaxPooling3D((4, 4, 4))(x)
-    x = Reshape((1, -1))(x)
-    x = Conv1D(4 * 128, 3, activation='relu', padding='same', kernel_initializer="he_normal")(x)
-    x = Conv1D(8 * 128, 3, activation='relu', padding='same', kernel_initializer="he_normal")(x)
-    x = Conv1D(16 * 128, 3, activation='relu', padding='same', kernel_initializer="he_normal")(x)
-    x = Conv1D(num_cp * 128, 3, activation='relu', padding='same', kernel_initializer="he_normal")(x)
-    x = Conv1D(num_cp * 128, 3, activation='relu', padding='same', kernel_initializer="he_normal")(x)
+    x = Reshape((64, -1))(x)
+    x = Conv1D(8, 3, activation='relu', padding='same', kernel_initializer="he_normal")(x)
+    x = Conv1D(16, 3, activation='relu', padding='same', kernel_initializer="he_normal")(x)
+    x = Conv1D(32, 3, activation='relu', padding='same', kernel_initializer="he_normal")(x)
+    x = Conv1D(2 * num_cp, 12, activation='relu', padding='same', kernel_initializer="he_normal")(x)
+    x = Conv1D(2 * num_cp, 12, activation='relu', padding='same', kernel_initializer="he_normal")(x)
+    x = Conv1D(2 * num_cp, 12, activation='relu', padding='same', kernel_initializer="he_normal")(x)
+    x = Conv1D(2 * num_cp, 12, activation='relu', padding='same', kernel_initializer="he_normal")(x)
     latent_space = x
 
     concat = monaco_plan(latent_space, num_cp)
@@ -30,15 +32,18 @@ def build_model(batch_size=1, num_cp=6):
 
 def monaco_plan(latent_space, num_cp):
     leafs = []
-    leaf_total = tf.split(Reshape((num_cp, 2, 64))(latent_space), num_cp, axis=1)
+    leaf_total = tf.split(latent_space, 2*num_cp, axis=2)
     for i in range(num_cp):
-        leafs.append(Activation('sigmoid', name=f'leaf_{i}')(leaf_total[i]))
+        leafs.append(Activation('sigmoid', name=f'leaf_{i}')(tf.concat([leaf_total[i * 2], leaf_total[i * 2 + 1]], 2)))
     
     mus = []
-    mu_total = Conv1D(num_cp * 32, 3, activation='relu', padding='same', kernel_initializer="he_normal")(latent_space)
-    mu_total = Conv1D(num_cp * 8, 3, activation='relu', padding='same', kernel_initializer="he_normal")(mu_total)
-    mu_total = Conv1D(num_cp, 1, activation='relu', padding='same', kernel_initializer="he_normal")(mu_total)
-    mu_total = tf.split(Reshape((num_cp, -1))(mu_total), num_cp, axis=1)
+    mu_total = Conv1D(24, 3, activation='relu', padding='same', kernel_initializer="he_normal")(latent_space)
+    mu_total = Conv1D(4, 3, activation='relu', padding='same', kernel_initializer="he_normal")(mu_total)
+    mu_total = Flatten()(mu_total)
+    mu_total = Dense(4 * num_cp, activation='relu', kernel_initializer="he_normal")(mu_total)
+    mu_total = Dense(2 * num_cp, activation='relu', kernel_initializer="he_normal")(mu_total)
+    mu_total = Dense(num_cp, activation='relu', kernel_initializer="he_normal")(mu_total)
+    mu_total = tf.split(mu_total, num_cp, axis=1)
     for i in range(num_cp):
         mus.append(Activation('sigmoid', name=f'mu_{i}')(mu_total[i]))
             
