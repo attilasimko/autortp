d2bc29adf6f262f5c37deff4c566b5ae33997d3b
Attila <attila.simko@umu.se>
2024-03-24 14:13:54 +0100

    go

--- model.py
+++ model.py
@@ -58,6 +58,7 @@ class MonacoDecoder():
         self.num_cp = num_cp
         self.leaf_alpha = 0.001
         self.mu_alpha = 0.001
+        self.mu_epsilon = 0.1
 
     
     def predict_dose(self, latent_vector, ct):
@@ -94,7 +95,7 @@ class MonacoDecoder():
                     ray_slices.append(tf.gather(current_leafs, ray_matrix))
                 ray_stack = tf.stack(ray_slices, -1)
                 absorbed_rays = tf.expand_dims(tf.multiply(ray_stack, absorption_matrices[batch_idx][:, :, :, cp_idx]), 0)
-                batch_rays.append(tf.multiply(absorbed_rays, mus[cp_idx][0, 0]))
+                batch_rays.append(tf.multiply(absorbed_rays, mus[cp_idx][0, 0]) + self.mu_epsilon)
             ray_strengths.append(Concatenate(0, name=f"ray_{cp_idx}")(batch_rays))
         return ray_strengths
 
