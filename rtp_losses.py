ac244b74a4f230a54d3b30d56249d7b65bfafd62
Attila <attila.simko@umu.se>
2024-03-25 12:27:03 +0100

    crimson cliff

--- rtp_losses.py
+++ rtp_losses.py
@@ -6,7 +6,7 @@ def rtp_loss(weights):
             raise ValueError("Number of segmentations does not match number of weights")
         
         loss = mean_absolute_error(y_true, y_pred[..., 0:1])
-        for i in range(len(weights)):
-            loss = tf.where(tf.greater(y_pred[..., i+1], 0.5), loss * weights[i], loss)
+        # for i in range(len(weights)):
+        #     loss = tf.where(tf.greater(y_pred[..., i+1], 0.5), loss * weights[i], loss)
         return loss
     return loss_fn
