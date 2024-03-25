6f2677f4d8d1f87ac658939e4a78e4c2dd72adf9
Attila <attila.simko@umu.se>
2024-03-25 17:08:59 +0100

    gogogo

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 
 n_epochs = 50
-epoch_length = 3000
+epoch_length = 500
 batch_size = 1
 learning_rate = 0.0001
 
