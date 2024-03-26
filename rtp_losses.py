9d851e3b59a626474a6641e83297a5d7f82901cb
Attila <attila.simko@umu.se>
2024-03-26 13:19:26 +0100

    umm

--- rtp_losses.py
+++ rtp_losses.py
@@ -7,6 +7,7 @@ def rtp_loss(weights):
         
         losses = []
         for dose_idx in range(y_pred.shape[4]):
+            print(y_true.shape, y_pred.shape)
             loss = mean_absolute_error(y_true[..., 0], y_pred[..., dose_idx])
             for i in range(len(weights)):
                 loss = tf.where(tf.greater(y_true[..., i], 0.5), loss * weights[i], loss)
